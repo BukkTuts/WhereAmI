e2716bdd040c016c948bc073a51e3830d1be396a
drtshock <thensler@wisc.edu>
2013-04-10 17:35:10 -0500

    spaces :o

--- src/com/bukkthat/whereami/WhereAmI.java
+++ src/com/bukkthat/whereami/WhereAmI.java
@@ -8,18 +8,18 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class WhereAmI extends JavaPlugin {
 
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if(!(sender instanceof Player)) {
-			sender.sendMessage("Only players have locations D:");
-		} else {
-			Player player = (Player) sender;
-			double x = player.getLocation().getX();
-			double y = player.getLocation().getY();
-			double z = player.getLocation().getZ();
-			String world = player.getWorld().getName();
-			player.sendMessage(ChatColor.GOLD + "X: " + x + ", Y: " + y + ", Z: " + z + ", World: " + world);
-		}
-		return true;
-	}
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if(!(sender instanceof Player)) {
+            sender.sendMessage("Only players have locations D:");
+        } else {
+            Player player = (Player) sender;
+            double x = player.getLocation().getX();
+            double y = player.getLocation().getY();
+            double z = player.getLocation().getZ();
+            String world = player.getWorld().getName();
+            player.sendMessage(ChatColor.GOLD + "X: " + x + ", Y: " + y + ", Z: " + z + ", World: " + world);
+        }
+        return true;
+    }
 
 }
